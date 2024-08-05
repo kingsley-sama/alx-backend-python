c98d465917c80d9f191d759d35d3f067e1ceca78
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-05 14:35:27 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-"""this module defines and asyncronous coroutine that calls a function n times"""
+"""this module defines and asyncronous
+ecoroutine that calls a function n times
+"""
 import asyncio
 import random
 from typing import List
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
