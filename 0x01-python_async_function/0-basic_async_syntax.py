0ca9210eafc6c60dceb0a208393cccbad6b53118
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-05 13:50:33 +0100

    removes awaitable types in 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """this module defines and asyncronous coroutine that takes in an integer"""
 import asyncio
-from typing import Awaitable
 import random
 
 
-async def wait_random(max_delay: int = 10) -> Awaitable[float]:
+async def wait_random(max_delay: int = 10) -> float:
     """ asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that waits
     """
