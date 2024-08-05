5b873df9bb5960a21005f8c80e04ee443d45e2f6
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-05 12:46:22 +0100

    initial commit

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """this module defines and asyncronous coroutine that takes in an integer"""
 import asyncio
+from typing import Awaitable
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> Awaitable[float]:
     """ asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that waits
     """
