b6449924312ccc04d15b2eb3aeccf8142837e2e6
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-06 23:02:05 +0100

    Updates subscript to type List

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,7 +11,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     '''wait_n calls a function n times as a routine'''
     coroutines = [wait_random(max_delay) for i in range(n)]
     result = await asyncio.gather(*coroutines)
