768a10e8759908ab4a0a2aba92955fd8f4d58538
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-26 16:22:28 +0100

    updates test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,9 +7,8 @@ from typing import (
     Mapping,
     Sequence,
     Any,
-    Dict,
-    Callable,
-)
+    )
+
 
 
 class TestMemoize(unittest.TestCase):
@@ -20,11 +19,9 @@ class TestMemoize(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map: Mapping, path:
-                               Sequence, expected_output: Any):
+                               Sequence, expected_output: Any) -> None:
         """test suits for access_nested_map
         """
         result: Any = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_output)
 
-
-test = TestMemoize()
