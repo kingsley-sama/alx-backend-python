387c7cb3f8724083526c1c613ff8814ccd38970d
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-27 15:02:49 +0100

    fixes decororator issue in test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,4 +19,4 @@ class TestAccessNestedMap(unittest.TestCase):
         ])
     def test_access_nested_map(self, nested_map, path, expected_output):
         """test suits for access_nested_map """
-        self.assertEqual(qaccess_nested_map(nested_map, path), expected_output)
+        self.assertEqual(access_nested_map(nested_map, path), expected_output)
