a6c18ac31cc473ac6234f4f434cd35ce315b8b54
Kingsley Okpo <kngsley2018@gmail.com>
2024-08-27 16:00:55 +0100

    creates function to test keyerror in test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,10 @@ from typing import (
     Any
 )
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Parameterize and patch"""
-    
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
@@ -20,3 +21,12 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected_output):
         """test suits for access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """test suits for access_nested_map raises keyerror """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
